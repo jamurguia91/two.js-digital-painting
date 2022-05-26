5c075d0a23d72c006334a9bb9850cd97af0901c8
jamurguia91 <80859057+jamurguia91@users.noreply.github.com>
2022-05-26 15:44:04 -0700

    update mostly js
    got rid of borrowed git js code and replaced with in class example for now

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-// borrowed from the two js github page
-// supposed to be a spinning rectangle
-var two = new Two({
-    fullscreen: true,
-    autostart: true
-  }).appendTo(document.body);
-  var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
-  two.bind('update', function() {
-    rect.rotation += 0.001;
-  });
+
+const container = document.querySelector("section");
+
+// setting the dimensions for the container
+let params = {
+    width: 500,
+    ehight: 500
+};
+
+// `new` instantiation Two.js
+const two = new Two(params);
+// append to `container`
+two.appendTo(container);
+
+// shape to be moving
+let shape = two.makeRectangle(250,250,100,100);
+
+// PROPERTY: set fill of shape
+shape.fill = "Cornsilk";
+// METHOD: run method to remove stroke
+shape.noStroke();
+
+// rotate shape. Default is radians
+shape.rotation = Math.PI * 0.25;
+
+// Bind to "Update" function
+two.bind("update", () => {
+  shape.rotation += 0.01;
+});
+
+// Draw to page
+two.play();
